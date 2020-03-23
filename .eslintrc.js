96a4522580c33fb1e82165f6a368f9522474accc
Thomas Reynolds <me@tdreyno.com>
2020-03-23 10:23:26 -0700

    style: align with standard eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,23 @@
 module.exports = {
   env: {
     browser: true,
-    es6: true
+    es6: true,
   },
   extends: [
     "plugin:prettier/recommended",
     "plugin:@typescript-eslint/recommended",
-    "plugin:@typescript-eslint/recommended-requiring-type-checking"
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: "tsconfig.json",
-    sourceType: "module"
+    sourceType: "module",
   },
   plugins: ["@typescript-eslint"],
-  rules: { "@typescript-eslint/explicit-function-return-type": 0 }
+  rules: {
+    "@typescript-eslint/explicit-function-return-type": 0,
+    "@typescript-eslint/require-await": 0,
+    semi: ["error", "never"],
+    "@typescript-eslint/member-delimiter-style": 0,
+  },
 }
